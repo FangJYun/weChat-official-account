7c004fdf99a4f252dc03bd685ad487d3f85ab84f
fangjiayun <1071875401@qq.com>
2025-01-04 18:37:38 +0800

    用例更新

--- dockercompose-demo/app.py
+++ dockercompose-demo/app.py
@@ -1,12 +1,11 @@
 import time
-​
 import redis
 from flask import Flask
-​
+
 app = Flask(__name__)
 # host='redis',其中redis是service的名称，compose中的服务通信可以用服务名来指定
 cache = redis.Redis(host='redis', port=6379)
-​
+
 def get_hit_count():
     retries = 5
     while True:
@@ -17,8 +16,8 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-​
+
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.\n'
+    return f'Hello Python! I have been seen {count} times.\n'
